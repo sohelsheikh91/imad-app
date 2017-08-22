bd4fd1e42830af0cb5dd9aa7ca97f2685180582d
sohelsheikh91 <31231936+sohelsheikh91@users.noreply.github.com>
2017-08-22 23:53:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.get('/article-one', function (req, res){
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res){
-    res.send('Article three requested and will served here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   
 });
 app.get('/article-three', function (req, res){
-    res.send('Article four requested and will served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
