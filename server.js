3a8c506d8300767320a6dec1b8e81a8eea8356c0
sohelsheikh91 <31231936+sohelsheikh91@users.noreply.github.com>
2017-08-23 09:58:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res){
-    var articleName = req.params.articlename;
-res.send(createTemplate(articles[articlename]));
+    var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
