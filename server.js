4d3757cc281340c81f8b60fc8c90c86eb3dcc564
sohelsheikh91 <31231936+sohelsheikh91@users.noreply.github.com>
2017-08-26 22:45:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,13 +101,13 @@ return htmlTemplate;
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool =new Pool (config);
+var pool =new Pool(config);
 app.get('/test-db',function (req,res){
     pool.query('SELECT = FROM test', function(err , result){
         if (err){
             res.status(500).send(err.toString());
         }else {
-            red.send(JSON.stringify(result));
+            res.send(JSON.stringify(result));
             
         }
     });
