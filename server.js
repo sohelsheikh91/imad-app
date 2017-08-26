519a3f6f472b775829c3903ad4135b9e33357422
sohelsheikh91 <31231936+sohelsheikh91@users.noreply.github.com>
2017-08-26 22:40:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool =require('pg').Pool;
+var config = {
+    user: 'sohelsheikh',
+    database: 'sohelsheikh91',
+    host: 'db.imad.hausura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -92,6 +100,17 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool =new Pool (config)
+app.get('/test-db',function (req,res){
+    pool.query('SELECT = FROM test', function(err , result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else {
+            red.send(JSON.stringify(result));
+            
+        }
+    });
 });
 var counter = 0;
 app.get('/counter', function (req, res) {
